260f315080d1176b2636a45ecbddf677267764b8
teglev <teglev.sky@gmail.com>
2022-11-22 00:09:35 +0300

    modify brain-calc

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -4,19 +4,20 @@ MAIN_QUESTION = 'What is the result of the expression?'
 
 
 def make_calculation(first_operand, operator, second_operand):
-    question = f'{first_operand} {operator} {second_operand}'
     if operator == '*':
-        correct_answer = first_operand * second_operand
+        result = first_operand * second_operand
     elif operator == '-':
-        correct_answer = first_operand - second_operand
+        result = first_operand - second_operand
     elif operator == '+':
-        correct_answer = first_operand + second_operand
-    correct_answer = str(correct_answer)
-    return [question, correct_answer]
+        result = first_operand + second_operand
+    result = str(result)
+    return [result]
 
 
 def run_game():
     first_operand = randint(0, 100)
     second_operand = randint(0, 100)
     operator = choice(['*', '-', '+'])
-    return make_calculation(first_operand, operator, second_operand)
+    question = f'{first_operand} {operator} {second_operand}'
+    correct_answer = make_calculation(first_operand, operator, second_operand)
+    return [question, correct_answer]
