86958e566d6c28cd5b208c207c60f6f2e69c5d43
teglev <teglev.sky@gmail.com>
2022-11-21 14:53:34 +0300

    correction in brain-primt

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -6,9 +6,8 @@ main_question = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def colculation():
   condtions = randint(1, 10000)
+  result = 'yes'
   for i in range(2, (condtions//2)+1):
-        if condtions % i == 0:
+    if condtions % i == 0:
           result = 'no'
-        else:
-          result = 'yes'
   return [condtions, result]
