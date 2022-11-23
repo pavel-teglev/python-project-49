eb936d87673dab066567a8a02843da90ab7463ed
teglev <teglev.sky@gmail.com>
2022-11-23 10:00:04 +0300

    fix brain-prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -6,6 +6,8 @@ CONDITIONS = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def is_prime(number):
     result = True
+    if number < 2:
+        result = False
     for i in range(2, (number // 2) + 1):
         if number % i == 0:
             result = False
